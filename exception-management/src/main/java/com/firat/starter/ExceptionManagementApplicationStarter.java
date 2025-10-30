79d10129f02e217acdda20bcd04c2f3a2349520a
Firatasi <firat_asi@icloud.com>
2025-10-30 13:32:39 +0300

    schedule(çalışmasını istediğin zaman çalıştırma ve kaç defa çalışacağını belirleme)

--- exception-management/src/main/java/com/firat/starter/ExceptionManagementApplicationStarter.java
+++ exception-management/src/main/java/com/firat/starter/ExceptionManagementApplicationStarter.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.firat"})
 @EnableJpaRepositories(basePackages = {"com.firat"})
 @ComponentScan(basePackages = {"com.firat"})
 @SpringBootApplication
+@EnableScheduling //schedule çalışması için anatasyonu burda çağırıyoruz
 public class ExceptionManagementApplicationStarter {
 
     public static void main(String[] args) {
