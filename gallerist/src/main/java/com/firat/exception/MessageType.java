42619c66f142a3207e0c954f906b0afeafc62753
Firatasi <firat_asi@icloud.com>
2025-12-02 16:48:01 +0300

    1-jwt Service, 2-jwtAuhtentication sınıfları kodlanması

--- gallerist/src/main/java/com/firat/exception/MessageType.java
+++ gallerist/src/main/java/com/firat/exception/MessageType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum MessageType {
     GENERAL_EXCEPTION("9999", "Genel bir hata oluştu! "),
+    TOKEN_IS_EXPIRED("1005", "Token has expired!"),
     NO_RECORD_EXIST("1004", "Kayıt bulunamadı! ");
 
     private String code;
---
+++ gallerist/src/main/java/com/firat/jwt/JWTAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.firat.jwt;
+
+import com.firat.exception.BaseException;
+import com.firat.exception.ErrorMessage;
+import com.firat.exception.MessageType;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+        if (header == null) {
+            filterChain.doFilter(request, response); //token gönderilmemişse
+            return;
+        }
+        String token;
+        String username;
+
+        token = header.substring(7);//7. karakterden sonra tokenı verir tokenı al
+
+        try {
+            username = jwtService.getUsernameByToken(token);
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null) { //contexe herhangi bir kullanıcı setlenmediyse
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (userDetails != null && jwtService.isTokenValid(token)) {
+                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                    authenticationToken.setDetails(userDetails);
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+
+        } catch(ExpiredJwtException ex) {
+            throw new BaseException(new ErrorMessage(ex.getMessage(), MessageType.TOKEN_IS_EXPIRED));
+        } catch (Exception e){
+            throw new BaseException(new ErrorMessage(e.getMessage(), MessageType.GENERAL_EXCEPTION));
+
+        }
+
+
+    }
+}
