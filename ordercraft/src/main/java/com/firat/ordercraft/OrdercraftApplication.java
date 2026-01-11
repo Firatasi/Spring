1bdc06cd862d927819953bb8aa728a5c33fa4120
Firatasi <firat_asi@icloud.com>
2026-01-11 18:36:25 +0300

    ordercraft

---
+++ ordercraft/src/main/java/com/firat/ordercraft/OrdercraftApplication.java
@@ -0,0 +1,13 @@
+package com.firat.ordercraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdercraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdercraftApplication.class, args);
+    }
+
+}
---
+++ ordercraft/src/test/java/com/firat/ordercraft/OrdercraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firat.ordercraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdercraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
