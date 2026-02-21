9a772cb337b8b0558204e2b8158ed9cac5941172
Firatasi <firat_asi@icloud.com>
2026-02-21 13:02:48 +0300

    fix mikroservice

--- ulke_baskent/Surucu/src/main/java/com/demo/surucu/controller/SurucuControllerRestClient.java
+++ ulke_baskent/Surucu/src/main/java/com/demo/surucu/controller/SurucuControllerRestClient.java
@@ -1,11 +1,61 @@
 package com.demo.surucu.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.demo.surucu.CarResponse;
+import com.demo.surucu.service.SurucuServiceRestClient;
+import com.userpe.dto.request.CarRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/v1/surucu")
+import java.util.List;
+
+@RequestMapping("/rest-client-car")
 @RestController
 public class SurucuControllerRestClient {
+
+    private final SurucuServiceRestClient surucuServiceRestClient;
+
+    public SurucuControllerRestClient(SurucuServiceRestClient surucuServiceRestClient) {
+        this.surucuServiceRestClient = surucuServiceRestClient;
+    }
+
+    @GetMapping("/toEntity/{id}")
+    public ResponseEntity<CarResponse> getCar(@PathVariable Long id) {
+        ResponseEntity<CarResponse> car = surucuServiceRestClient.getCar(id);
+        return car;
+    }
+
+    @GetMapping("/body/{id}")
+    public ResponseEntity<CarResponse> getCar2(@PathVariable Long id) {
+        CarResponse carResponse = surucuServiceRestClient.getCar2(id);
+        return ResponseEntity.ok(carResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CarResponse>> getAllCars() {
+        ResponseEntity<List<CarResponse>> responseEntity = surucuServiceRestClient.getAllCar();
+        return responseEntity;
+    }
+
+    @PostMapping("/add1")
+    public ResponseEntity<CarResponse> addCar(CarRequest carRequest) {
+        return surucuServiceRestClient.addCar(carRequest);
+    }
+
+    @PostMapping("/add2")
+    public ResponseEntity<CarResponse> addCar2(CarRequest carRequest) {
+        CarResponse carResponse = surucuServiceRestClient.addCar2(carRequest);
+        return ResponseEntity.ok(carResponse);
+    }
+
+    @PostMapping("/add3")
+    public ResponseEntity<?> addCar3(CarRequest carRequest) {
+         surucuServiceRestClient.addCar3(carRequest);
+         return ResponseEntity.status(HttpStatus.CREATED).body("eklendi");
+    }
+
+
+
 
 
 
--- ulke_baskent/Surucu/src/main/java/com/demo/surucu/service/SurucuServiceRestClient.java
+++ ulke_baskent/Surucu/src/main/java/com/demo/surucu/service/SurucuServiceRestClient.java
@@ -1,10 +1,15 @@
 package com.demo.surucu.service;
 
 import com.demo.surucu.CarResponse;
+import com.userpe.dto.request.CarRequest;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @Service
 public class SurucuServiceRestClient {
@@ -15,9 +20,77 @@ public class SurucuServiceRestClient {
     }
 
     public ResponseEntity<CarResponse> getCar(Long id) {
-        return null;
+
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("/{id}")  BASE URL VAR İSE KULLANILMAZ
+//                .buildAndExpand(id)
+//                .toUri();
+
+        ResponseEntity<CarResponse> responseEntity = restClient
+                .get()
+                .uri("/{id}",id) //uri şablonu ve parametre
+//                .uri(id) base url yoksa direkt id yazılabilir yukarda uricomponent yapısı kullanılabilir
+                .retrieve() //isteği gönder ve yanıtı al
+                .toEntity(CarResponse.class); //json yanıtını java nesnesine çevirir,Status code (200, 404, 500…),Header bilgileri,Body
+
+        return responseEntity;
+
+    }
+
+    public CarResponse getCar2(Long id) {
+        CarResponse carResponse = restClient
+                .get()
+                .uri("/{id}", id)
+                .retrieve()
+                .body(CarResponse.class);//Bu JSON’u al, CarResponse nesnesine çevir.Status code, header bilgilerini vermez
+            return carResponse;
+    }
+
+    public void getCar3(Long id) {
+        restClient
+                .get()
+                .uri("/{id}",id)
+                .retrieve()
+                .toBodilessEntity(); // response’un body kısmını hiç okumadan, sadece status code + header bilgilerini döndürür.
+    }
+
+    public ResponseEntity<List<CarResponse>> getAllCar() {
+        ResponseEntity<List<CarResponse>> responseEntity = restClient
+                .get()
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<List<CarResponse>>() {
+                });
+                return responseEntity;
+    }
+
+    public ResponseEntity<CarResponse> addCar(CarRequest carRequest) {
+        ResponseEntity<CarResponse> entity = restClient
+                .post()
+                .header("Content-Type", "application/json")//post işlemleri için gerekli
+                .body(carRequest)
+                .retrieve()
+                .toEntity(CarResponse.class);
+        return entity;
     }
 
 
+    public CarResponse addCar2(CarRequest carRequest) {
+        CarResponse carResponse = restClient
+                .post()
+                .header("Content-Type", "application/json")//post işlemleri için gerekli
+                .body(carRequest)
+                .retrieve()
+                .body(CarResponse.class);
+        return carResponse;
+    }
+
+    public void addCar3(CarRequest carRequest) {
+        restClient
+                .post()
+                .header("Content-Type", "application/json")//post işlemleri için gerekli
+                .body(carRequest)
+                .retrieve()
+                .toBodilessEntity();
+    }
 
 }
