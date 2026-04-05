4d1fe96e34b277d7eb7e0cfdc4aa10e4989971e4
Firatasi <firat_asi@icloud.com>
2026-04-06 00:23:21 +0300

    43

--- realitygraph/src/main/java/com/demo/realitygraph/service/AnalysisService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.realitygraph.service;
-
-import com.demo.realitygraph.dto.AnalysisRequest;
-import com.demo.realitygraph.dto.AnalysisResponse;
-
-public interface  AnalysisService {
-    AnalysisResponse createAnalysis(AnalysisRequest request);
-    
-}
